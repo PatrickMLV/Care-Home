12824602b6429a2739ae487493a88400db5e01d3
Sharksa <sharksaBook@hotmail.com>
2013-02-19 20:45:48 +0100

    update Gmap with map center and opti

--- CareAtHome/web2/src/main/java/MapBean.java
+++ CareAtHome/web2/src/main/java/MapBean.java
@@ -1,5 +1,5 @@
-
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -18,85 +18,108 @@ import com.google.code.geocoder.GeocoderRequestBuilder;
 import com.google.code.geocoder.model.GeocodeResponse;
 import com.google.code.geocoder.model.GeocoderRequest;
 import com.google.code.geocoder.model.GeocoderResult;
+import com.google.code.geocoder.model.LatLngBounds;
 
- 
 @ManagedBean
 @SessionScoped
 public class MapBean implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -2315695442414431958L;
 
 	private MapModel simpleModel;
 
 	private Marker marker;
 	private String test = "Test d'affichage";
-	private double lat;
-	private double lon;
-
+	private double lat = 0;
+	private double lon = 0;
+	private double minLat = 9999;
+	private double minLon = 9999;
+	private double maxLat = 0;
+	private double maxLon = 0;	
+	private double zoom = 13;
+	
 	public String getTest() {
 		return test;
 	}
+
 	public double getLat() {
 		return lat;
-	}public double getLon() {
+	}
+
+	public double getLon() {
 		return lon;
 	}
 
-	public MapBean() {
-		simpleModel = new DefaultMapModel();
-		
-		//Shared coordinates
-		/*LatLng coord1 = new LatLng(36.879466, 30.667648);
-		LatLng coord2 = new LatLng(36.883707, 30.689216);
-		LatLng coord3 = new LatLng(36.879703, 30.706707);*/
-		LatLng coord4 = null;
-		LatLng coord5 = null;
-		
-		final Geocoder geocoder = new Geocoder();
-		GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress("5 Boulevard Descartes, 77454 Champs-sur-Marne").setLanguage("en").getGeocoderRequest();
-		GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
-		List<GeocoderResult> results = geocoderResponse.getResults();
-		for (GeocoderResult geocoderResult : results) {
-			lat = geocoderResult.getGeometry().getLocation().getLat().doubleValue();
-			lon = geocoderResult.getGeometry().getLocation().getLng().doubleValue();
-			coord4 = new LatLng(lat, lon);
-		}
-		
-		
-		geocoderRequest = new GeocoderRequestBuilder().setAddress("Aérodrome de Lognes-Emerainville, Darse B, Boulevard de Courcerin Lognes").setLanguage("en").getGeocoderRequest();
-		geocoderResponse = geocoder.geocode(geocoderRequest);
-		results = geocoderResponse.getResults();
-		for (GeocoderResult geocoderResult : results) {
-			coord5 = new LatLng(geocoderResult.getGeometry().getLocation().getLat().doubleValue(), geocoderResult.getGeometry().getLocation().getLng().doubleValue());
-		}
-		
-		//Basic marker
-		/*simpleModel.addOverlay(new Marker(coord1, "Konyaalti"));
-		simpleModel.addOverlay(new Marker(coord2, "Ataturk Parki"));
-		simpleModel.addOverlay(new Marker(coord3, "Karaalioglu Parki"));*/
-		simpleModel.addOverlay(new Marker(coord4, "Test Fac"));
-		simpleModel.addOverlay(new Marker(coord5, "Test Aerodrome"));
+	public double getZoom() {
+		return zoom;
 	}
 	
+	public MapBean() {
+		simpleModel = new DefaultMapModel();
+		String[] AdressList = {
+				"Copernic 5 Boulevard Descartes, 77454 Champs-sur-Marne",
+				"Aérodrome de Lognes-Emerainville, Boulevard de Courcerin Lognes",
+				"2 Boulevard Blaise Pascal 93162 Noisy-le-Grand Esiee" };
+		
+		final Geocoder geocoder = new Geocoder();
+		GeocoderRequest geocoderRequest = null;
+		GeocodeResponse geocodeResponse = null;
+		List<GeocoderResult> results = new ArrayList<GeocoderResult>();
+		LatLng coord = null;
+		
+		for (String string : AdressList) {
+			geocoderRequest = new GeocoderRequestBuilder().setAddress(string).setLanguage("fr").getGeocoderRequest();
+			geocodeResponse = geocoder.geocode(geocoderRequest);
+			results = geocodeResponse.getResults();
+			for (GeocoderResult geocoderResult : results) {
+				lat = geocoderResult.getGeometry().getLocation().getLat()
+						.doubleValue();
+				lon = geocoderResult.getGeometry().getLocation().getLng()
+						.doubleValue();
+				
+				if(lat < minLat){
+					minLat = lat;
+				}
+				if(lat > maxLat){
+					maxLat = lat;
+				}
+				if(lon < minLon){
+					minLon = lon;
+				}
+				if(lon > maxLon){
+					maxLon = lon;
+				}
+				coord = new LatLng(lat, lon);
+				
+				simpleModel.addOverlay(new Marker(coord, string));
+		 	}
+		}
+		
+		
+		/**
+		 * Calcul du centrage de la map pour voir tous les points
+		 */
+		lat = (minLat + maxLat)/2;
+		lon = (minLon + maxLon)/2;
+
+	}
+
 	public MapModel getSimpleModel() {
 		return simpleModel;
 	}
-	
+
 	public void onMarkerSelect(OverlaySelectEvent event) {
 		marker = (Marker) event.getOverlay();
-		
-		addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO, "Marker Selected", marker.getTitle()));
+
+		addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO,
+				"Marker Selected", marker.getTitle()));
 	}
-	
+
 	public Marker getMarker() {
 		return marker;
 	}
-	
+
 	public void addMessage(FacesMessage message) {
 		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 }
-					
