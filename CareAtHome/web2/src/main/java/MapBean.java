3260b0c0d80371dc880e7918942e94304cf9dbd9
Sharksa <sharksaBook@hotmail.com>
2013-02-21 23:47:58 +0100

    add zoom calcul in gmap

--- CareAtHome/web2/src/main/java/MapBean.java
+++ CareAtHome/web2/src/main/java/MapBean.java
@@ -18,7 +18,6 @@ import com.google.code.geocoder.GeocoderRequestBuilder;
 import com.google.code.geocoder.model.GeocodeResponse;
 import com.google.code.geocoder.model.GeocoderRequest;
 import com.google.code.geocoder.model.GeocoderResult;
-import com.google.code.geocoder.model.LatLngBounds;
 
 @ManagedBean
 @SessionScoped
@@ -55,10 +54,18 @@ public class MapBean implements Serializable {
 	}
 
 	public MapBean() {
-		users.add(new User("Fac", "MLV", "Copernic 5 Boulevard Descartes, 77454 Champs-sur-Marne", "0606060606"));
-		users.add(new User("Aerodrome", "Lognes", "Aérodrome de Lognes-Emerainville, Boulevard de Courcerin Lognes", "0707070707"));
-		users.add(new User("Ecole", "Esiee", "2 Boulevard Blaise Pascal 93162 Noisy-le-Grand Esiee", "0808080808"));
-		
+		users.add(new User("Fac", "MLV",
+				"Copernic 5 Boulevard Descartes, 77454 Champs-sur-Marne",
+				"0606060606"));
+		users.add(new User(
+				"Aerodrome",
+				"Lognes",
+				"Aérodrome de Lognes-Emerainville, Boulevard de Courcerin Lognes",
+				"0707070707"));
+		users.add(new User("Ecole", "Esiee",
+				"2 Boulevard Blaise Pascal 93162 Noisy-le-Grand Esiee",
+				"0808080808"));
+
 		simpleModel = new DefaultMapModel();
 
 		final Geocoder geocoder = new Geocoder();
@@ -67,9 +74,10 @@ public class MapBean implements Serializable {
 		List<GeocoderResult> results = new ArrayList<GeocoderResult>();
 		LatLng coord = null;
 
-		for(User user : users){
-			geocoderRequest = new GeocoderRequestBuilder().setAddress(user.getAdress())
-					.setLanguage("fr").getGeocoderRequest();
+		for (User user : users) {
+			geocoderRequest = new GeocoderRequestBuilder()
+					.setAddress(user.getAdress()).setLanguage("fr")
+					.getGeocoderRequest();
 			geocodeResponse = geocoder.geocode(geocoderRequest);
 			results = geocodeResponse.getResults();
 			for (GeocoderResult geocoderResult : results) {
@@ -95,7 +103,8 @@ public class MapBean implements Serializable {
 				simpleModel.addOverlay(new Marker(coord, user.getAdress()));
 			}
 		}
-		
+
+		zoomDefinition(minLat, minLon, maxLat, maxLon, users.size());
 
 		/**
 		 * Calcul du centrage de la map pour voir tous les points
@@ -103,6 +112,30 @@ public class MapBean implements Serializable {
 		lat = (minLat + maxLat) / 2;
 		lon = (minLon + maxLon) / 2;
 
+	}
+
+	public void zoomDefinition(double minLat, double minLon, double maxLat,
+			double maxLon, int marquerNumber) {
+		/**
+		 * Si on a qu'un marqueur on met par défaut le zoom a 13
+		 */
+		if (marquerNumber == 1 || ((minLat == maxLat) && (minLat == maxLat))) {
+			zoom = 13;
+		} else {
+
+			int mapdisplay = 322; 
+			double dist = (6371 * Math
+					.acos(Math.sin(minLat / 57.2958)
+							* Math.sin(maxLat / 57.2958)
+							+ (Math.cos(minLat / 57.2958)
+									* Math.cos(maxLat / 57.2958) * Math
+										.cos((maxLon / 57.2958)
+												- (minLon / 57.2958)))));
+			zoom = Math.floor(7
+					- Math.log(1.6446 * dist
+							/ Math.sqrt(2 * (mapdisplay * mapdisplay)))
+					/ Math.log(2));
+		}
 	}
 
 	public MapModel getSimpleModel() {
