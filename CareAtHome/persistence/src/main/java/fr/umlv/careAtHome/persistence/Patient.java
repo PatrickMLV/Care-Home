2fd9f41d1da8870b90293c7f6f84fd5520c763c0
Giry Geoffrey <ggiry@etudiant.univ-mlv.fr>
2013-03-04 02:37:19 +0100

    Ajout de l'arbitrage et de la couverture fonctionnelle

---
+++ CareAtHome/persistence/src/main/java/fr/umlv/careAtHome/persistence/Patient.java
@@ -0,0 +1,85 @@
+package fr.umlv.careAtHome.persistence;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Patient {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "UUID")
+    private int uuid;
+
+    @Column(name = "FIRSTNAME")
+	private String firstName;
+
+    @Column(name = "LASTNAME")
+    private String lastName;
+
+    @Column(name = "BIRTHDAY")
+    private Date birthday;
+
+    @Column(name = "SSID")
+    private String ssid;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "PHONE")
+    private String phone;
+	
+	public Patient(String firstName, String lastName, Date birthday,
+                   String ssid, String address, String phone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthday = birthday;
+		this.ssid = ssid;
+		this.address = address;
+		this.phone = phone;
+	}
+	public Patient() {
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getSsid() {
+		return ssid;
+	}
+	public void setSsid(String ssid) {
+		this.ssid = ssid;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
---
+++ CareAtHome/persistence/src/test/resources/import.sql
@@ -0,0 +1,4 @@
+insert into PATIENT(UUID, FIRSTNAME, LASTNAME, SSID, ADDRESS, PHONE) values (1, 'Jack', 'Nickolson', 123456, '5 Boulevard Descarte, 77454 Champs-sur-Marne', '0606060606')
+insert into PATIENT(UUID, FIRSTNAME, LASTNAME, SSID, ADDRESS, PHONE) values (2, 'Anthony', 'Hopkins', 123457, '2 Boulevard Blaise Pascal, 93162 Noisy-le-Grand Esiee', '0606060607')
+insert into PATIENT(UUID, FIRSTNAME, LASTNAME, SSID, ADDRESS, PHONE) values (3, 'Prenom', 'Nom', 123458, 'Aerodrome de Lognes-Emerainville, Boulevard de Courcerin Lognes', '0606060608')
+
