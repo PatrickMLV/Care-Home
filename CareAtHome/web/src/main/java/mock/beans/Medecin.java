22db896657cb9164dccc4b2d2a41c5a735c471ba
JCharles <jcharles@jcharles-Aspire-4810T.(none)>
2013-03-21 09:35:09 +0100

    Medecin, Pharmacie and Laboratory are Serializable

--- CareAtHome/web/src/main/java/mock/beans/Medecin.java
+++ CareAtHome/web/src/main/java/mock/beans/Medecin.java
@@ -1,5 +1,6 @@
 package mock.beans;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -10,7 +11,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table (name="MEDECIN")
-public class Medecin {
+public class Medecin implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue
